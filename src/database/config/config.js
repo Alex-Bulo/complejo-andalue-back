39efe3dfa7ad3f6972c4e1be832800772203a646
Ale Bullo <bullorinia@gmail.com>
2022-02-27 19:09:40 -0300

    database config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
-    "username": db.USERNAME,
-    "password": db.PASSWORD,
-    "database": db.DATABASE,
-    "host": db.HOSTNAME,
-    "port":db.PORT,
-    "dialect": "mysql"
+    "username": "root",
+    "password": null,
+    "database": "andalue_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port":3306
   },
   "test": {
     "username": "root",
